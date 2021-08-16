3bac202c917492bf47298552b105b6c4a08488b0
Diego G. Arigony <diegoarigony@hotmail.com>
2021-08-16 19:58:37 -0300

    Remove debug on AuthorizationService

--- api/app/Services/AuthorizationService.php
+++ api/app/Services/AuthorizationService.php
@@ -11,8 +11,6 @@ class AuthorizationService
         $request = Http::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
         $response = json_decode((string) $request->getBody());
 
-        dump($response->message);
-
         if ($response->message !== 'Autorizado') {
             return false;
         }
